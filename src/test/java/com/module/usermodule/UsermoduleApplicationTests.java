52b256e20571d1d3e2450a668c1a4a346b72d5e5
ki-hong <rlghd153@gmail.com>
2022-08-08 23:40:06 +0900

    Initial commit

---
+++ src/test/java/com/module/usermodule/UsermoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.usermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
